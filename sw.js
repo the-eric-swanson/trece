bd84d242b22dcadbf09baabf65be291193f4a959
the-eric-swanson <ericwswanson@gmail.com>
2026-02-16 23:48:38 -0800

    actually adding sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'trece-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Add links to any custom icons or sounds here
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets from Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
