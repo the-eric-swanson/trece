2bcada11c1e72dd499c5e6244c7ce82ad608f1c4
the-eric-swanson <ericwswanson@gmail.com>
2026-02-21 01:08:42 -0800

    longer cooldown

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trece-v9'; // Change this number every time you push big changes
+const CACHE_NAME = 'trece-v10'; // Change this number every time you push big changes
 
 self.addEventListener('install', (event) => {
   // Force the waiting service worker to become the active service worker
