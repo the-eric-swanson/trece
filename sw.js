1a7b9d504370d306d4492cc42451050a6a83bcbd
the-eric-swanson <ericwswanson@gmail.com>
2026-02-21 00:23:52 -0800

    responsible gaming fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trece-v6'; // Change this number every time you push big changes
+const CACHE_NAME = 'trece-v7'; // Change this number every time you push big changes
 
 self.addEventListener('install', (event) => {
   // Force the waiting service worker to become the active service worker
