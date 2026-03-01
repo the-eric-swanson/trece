43b74c3b0a763d6c666d687aa270e0c9765196eb
the-eric-swanson <ericwswanson@gmail.com>
2026-02-28 23:04:46 -0800

    the definitive mobile upgrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trece-v18'; // Change this number every time you push big changes
+const CACHE_NAME = 'trece-v19'; // Change this number every time you push big changes
 
 self.addEventListener('install', (event) => {
   // Force the waiting service worker to become the active service worker
